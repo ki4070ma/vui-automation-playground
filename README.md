655d60d3ad9ff1639196c9ccc81ad82c2dd478d2
Atsushi Mori <ki4070ma@gmail.com>
2019-03-20 23:19:01 +0900

    Added speech to text

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 # Development
 
+## Text to speech
+
 ```pip install pyttsx3 pyobjc```
+
+## Speech to text
+
+```pip install SpeechRecognition```
+```brew install portaudio```
+```pip install pyaudio```
+
+### Trial on terminal
+```python -m speech_recognition```
+
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
+import pyttsx3
 
+
+def text_to_speech(sentence):
+    engine = pyttsx3.init()
+    engine.say(sentence)
+    engine.runAndWait()
+
+def speech_to_text():
+    import speech_recognition as sr
+
+    # get audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak:")
+        audio = r.listen(source, timeout=5)
+
+    try:
+        print("You said " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
 
 if __name__ == '__main__':
-    import pyttsx3
-    engine = pyttsx3.init()
-    # engine.say('Good morning.')
-    # engine.say('How are you?')
-    # engine.say('こんにちは')
-    engine.say('OK Google 明日の天気を教えて')
-    engine.runAndWait()
+    # text_to_speech('Good morning')
+    # text_to_speech('How are you?')
+    # text_to_speech('OK Google 明日の天気を教えて')
+
+    speech_to_text()
