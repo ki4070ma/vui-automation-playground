3f3e14904aeb54bb2cb60f5ec1c41118d1df5286
Atsushi Mori <ki4070ma@gmail.com>
2019-10-20 15:43:01 +0900

    Move helper methods to helper file

--- as_appium_extension/test_sample.py
+++ as_appium_extension/test_sample.py
@@ -3,61 +3,7 @@
 import os
 import unittest
 
-from appium import webdriver
-from util import GlobalVar
-
-
-# Returns abs path relative to this file and not cwd
-def PATH(p):
-    return os.path.abspath(
-        os.path.join(os.path.dirname(__file__), p)
-    )
-
-
-caps = {
-    'platformName': "Android",
-    'platformVersion': "8.0",
-    # 'platformVersion': "9",
-    'deviceName': "Android Emulator",
-    'appPackage': "org.gnucash.android",
-    'appActivity': ".ui.account.AccountsActivity",
-    'automationName': "uiautomator2",
-}
-
-
-class BaseTest(unittest.TestCase):
-    def __init__(self, method_name: str) -> None:
-        super(BaseTest, self).__init__(method_name)
-        self.caps = caps
-
-    def setUp(self) -> None:
-        self.driver = webdriver.Remote(
-            'http://localhost:4723/wd/hub', self.caps)
-
-        # Start taking evidence
-        self.make_log_dir(self._testMethodName)
-        self.driver.start_recording_screen()
-
-    def tearDown(self) -> None:
-        # Stop taking evidence
-        payload = self.driver.stop_recording_screen()
-        with open(os.path.join(GlobalVar().log_dir, "cap.mp4"), "wb") as fd:
-            import base64
-            fd.write(base64.b64decode(payload))
-
-        with open(os.path.join(GlobalVar().log_dir, "log.txt"), "w") as fd:
-            logs = self.driver.get_log('logcat')
-            for lines in logs:
-                for line in lines['message'].split('¥n'):
-                    fd.write(line + '\n')
-
-        # end the session
-        self.driver.quit()
-
-    @staticmethod
-    def make_log_dir(dir_name: str) -> None:
-        GlobalVar().log_dir = os.path.join(GlobalVar().log_root_dir, dir_name)
-        os.path.isdir(GlobalVar().log_dir) or os.makedirs(GlobalVar().log_dir)
+from helper.test_helper import PATH, BaseTest, GlobalVar
 
 
 class SampleAndroidTest(BaseTest):
