57471558ad263cdfbedc93a0e8f43b143ae947c0
Atsushi Mori <ki4070ma@gmail.com>
2019-11-18 00:16:19 +0900

    Update test cases

--- test_google_assistant_mobile_no_voice.py
+++ test_google_assistant_mobile_no_voice.py
@@ -13,7 +13,21 @@ class TestGoogleAssistant(BaseTestNoVoice):
 
     def test_whats_the_weather(self):
 
-        # ***Ask weather
-        self._say("what's the weather tomorrow")
+        self.driver.find_element_by_id(
+            'com.google.android.googlequicksearchbox:id/logo_view').click()
+
+        import time
+        time.sleep(5)
+
+        self.driver.find_element_by_id(
+            'com.google.android.googlequicksearchbox:id/keyboard_indicator').click()
+        time.sleep(5)
+
+        el = self.driver.find_element_by_id(
+            'com.google.android.googlequicksearchbox:id/input_text')
+        el.send_keys("what's the weather tomorrow")
+
+        self.driver.find_element_by_id(
+            'com.google.android.googlequicksearchbox:id/explore_icon_container').click()
 
         time.sleep(5)
