09cdcc1699b335e04730d8100b999a34dd23094a
Atsushi Mori <ki4070ma@gmail.com>
2019-11-18 01:31:50 +0900

    Add JP test cases without voice

--- test_google_assistant_mobile_jp_no_voice.py
+++ test_google_assistant_mobile_jp_no_voice.py
@@ -11,8 +11,8 @@ TIMEOUT = 10
 class TestGoogleAssistant(BaseTestNoVoice):
 
     # Format comes from http://appium.io/docs/en/writing-running-appium/caps/
-    LANG = 'en'
-    LOCALE = 'US'
+    LANG = 'ja'
+    LOCALE = 'JP'
 
     @classmethod
     def setup_class(cls):
@@ -21,17 +21,11 @@ class TestGoogleAssistant(BaseTestNoVoice):
     def test_whats_the_weather(self):
         self.open_chat_mode()
 
-        self.input_text("what's the weather tomorrow")
+        self.input_text(
+            self.s.get_sentence(
+                "weather",
+                "{}_{}".format(
+                    self.LANG,
+                    self.LOCALE)))
 
         time.sleep(5)
-
-    def test_open_music(self):
-        self.open_chat_mode()
-
-        self.input_text("Launch music")
-
-        for _ in range(TIMEOUT):  # TODO Make better
-            time.sleep(1)
-            if self.driver.current_package in TARGET_MUSIC_PKG:
-                break
-        assert self.driver.current_package in TARGET_MUSIC_PKG
