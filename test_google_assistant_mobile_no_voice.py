3b6eb8dbee0b72660923611f6b6fdbcf36b8c1df
Mori Atsushi <ki4070ma@gmail.com>
2019-11-18 00:56:40 +0900

    Add test case without tts: Launch music (#17)

--- test_google_assistant_mobile_no_voice.py
+++ test_google_assistant_mobile_no_voice.py
@@ -4,7 +4,7 @@ import time
 
 from helper.basetest_no_voice import BaseTestNoVoice
 
-TARGET_MUSIC_PKG = 'com.miui.player'
+TARGET_MUSIC_PKG = ['com.miui.player', 'com.google.android.music']
 TIMEOUT = 10
 
 
@@ -16,3 +16,14 @@ class TestGoogleAssistant(BaseTestNoVoice):
         self.input_text("what's the weather tomorrow")
 
         time.sleep(5)
+
+    def test_open_music(self):
+        self.open_chat_mode()
+
+        self.input_text("Launch music")
+
+        for _ in range(TIMEOUT):  # TODO Make better
+            time.sleep(1)
+            if self.driver.current_package in TARGET_MUSIC_PKG:
+                break
+        assert self.driver.current_package in TARGET_MUSIC_PKG
